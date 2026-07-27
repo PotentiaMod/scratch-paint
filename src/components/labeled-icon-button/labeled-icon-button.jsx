1859c08ecd509bb910c88505129bb18459587190
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-07-27 03:33:29 -0400

    a

--- src/components/labeled-icon-button/labeled-icon-button.jsx
+++ src/components/labeled-icon-button/labeled-icon-button.jsx
@@ -7,6 +7,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Button from '../button/button.jsx';
+import TWRenderRecoloredImage from '../../tw-recolor/render.jsx';
 
 import styles from './labeled-icon-button.css';
 
@@ -24,7 +25,7 @@ const LabeledIconButton = ({
         onClick={onClick}
         {...props}
     >
-        <img
+        <TWRenderRecoloredImage
             alt={imgAlt || title}
             className={styles.editFieldIcon}
             draggable={false}
