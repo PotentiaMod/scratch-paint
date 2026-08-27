a9b6cc49ff9f5a0e6769481acbe389fc701902aa
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-08-26 22:58:02 -0400

    UPLOAD CODE

--- src/components/mode-tools/mode-tools.jsx
+++ src/components/mode-tools/mode-tools.jsx
@@ -77,6 +77,8 @@ import bitRectIcon from '../bit-rect-mode/rectangle.svg';
 import bitOvalOutlinedIcon from '../bit-oval-mode/oval-outlined.svg';
 import bitRectOutlinedIcon from '../bit-rect-mode/rectangle-outlined.svg';
 import TWRenderRecoloredImage from '../../tw-recolor/render.jsx';
+import dashArrayIcon from './icons/dash-array.svg';
+import text2PathIcon from '!../../tw-recolor/build!./icons/text-to-shapes.svg';
 
 const LiveInput = LiveInputHOC(Input);
 const ModeToolsComponent = props => {
@@ -250,6 +252,16 @@ const ModeToolsComponent = props => {
             defaultMessage: 'Center Align',
             description: 'Label for the button that sets text alignment to the center',
             id: 'pm.paint.modeTools.centerAlign'
+        },
+		text2Path: {
+            defaultMessage: 'Text to Shapes',
+            description: 'Label for the button that converts text to shapes',
+            id: 'pm.paint.modeTools.text2Path'
+        },
+        more: {
+            defaultMessage: 'More',
+            description: 'Label for dropdown to access more action buttons',
+            id: 'paint.paintEditor.more'
         }
     });
 
@@ -847,6 +859,14 @@ const ModeToolsComponent = props => {
                             onClick={props.onTextAlignRight}
                         />
                     </InputGroup>
+					{(props.mode === Modes.TEXT) ? (<InputGroup className={classNames(styles.modDashedBorder, styles.modLabeledIconHeight)}>
+                        <LabeledIconButton
+                            hideLabel={hideLabel(props.intl.locale)}
+                            imgSrc={text2PathIcon}
+                            title={props.intl.formatMessage(messages.text2Path)}
+                            onClick={props.onText2Path}
+                        />
+                    </InputGroup>) : null}
                 </div>
             );
         case Modes.BIT_RECT:
@@ -950,6 +970,7 @@ ModeToolsComponent.propTypes = {
     onPasteFromClipboard: PropTypes.func.isRequired,
     onPointPoints: PropTypes.func.isRequired,
     onUpdateImage: PropTypes.func.isRequired,
+	onText2Path: PropTypes.func.isRequired,
 
     onMergeShape: PropTypes.func.isRequired,
     onMaskShape: PropTypes.func.isRequired,
